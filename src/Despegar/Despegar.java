ae48ef99bd2b1a8048ca4085c93b3c7e7fcb7367
Emmanuel Volpi Aranda <106482431+emmava03@users.noreply.github.com>
2022-05-30 20:01:33 -0300

    Correcciones

--- src/Despegar/Despegar.java
+++ src/Despegar/Despegar.java
@@ -16,51 +16,65 @@ public class Despegar
 		driver.manage().window().maximize();
 		driver.get("https://www.despegar.com.ar/");
 		WebElement Hoteles = driver
-				.findElement(By.cssSelector(".header-products-container [href='//www.despegar.com.ar/hoteles/']"));
+				.findElement(By.xpath("/html/body/nav/div[2]/div/div[3]/ul/li[1]/a/label"));
 		Hoteles.click();
 		Thread.sleep(1000);
 		WebElement imputTag = driver.findElement(
-				By.xpath("//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div[2]/div[1]/div/div/div/input"));
+				By.xpath("/html/body/div[4]/div/div/div[2]/div/div/div[2]/div[1]/div/div/div/input"));
 		Assert.assertTrue(imputTag.isDisplayed());
 		imputTag.click();
 		Thread.sleep(1000);
-		imputTag.sendKeys("Córdoba");
+		imputTag.sendKeys("Buenos");
 		Thread.sleep(1000);
 		WebElement TituloResultado = driver.findElement(By.xpath("/html/body/div[12]/div/div[1]/ul/li[1]/span"));
 		TituloResultado.click();
 		WebElement EcalendarDespegarIr = driver.findElement(By.xpath(
-				"//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div/input"));
+				"/html/body/div[4]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div/div/input"));
 		EcalendarDespegarIr.click();
 		Thread.sleep(1000);
 		WebElement FechaEntradaDespegarIr = driver.findElement(
-				By.xpath("//*[@id=\"component-modals\"]/div[4]/div[1]/div[2]/div[1]/div[3]/div[23]/div[1]"));
+				By.xpath("/html/body/div[6]/div[4]/div[1]/div[2]/div[2]/div[3]/div[14]/div"));
 		FechaEntradaDespegarIr.click();
 		Thread.sleep(1000);
 		WebElement FechaSalidaDespegarIr = driver
-				.findElement(By.xpath("//*[@id=\"component-modals\"]/div[4]/div[1]/div[2]/div[2]/div[3]/div[2]/div"));
+				.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[2]/div[2]/div[3]/div[10]/div"));
 		FechaSalidaDespegarIr.click();
 		Thread.sleep(1000);
 		WebElement aplicarFecha = driver
-				.findElement(By.xpath("//*[@id=\"component-modals\"]/div[4]/div[2]/div[1]/button/em"));
+				.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/button/em"));
 		aplicarFecha.click();
 		WebElement Hab = driver
-				.findElement(By.xpath("//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div[2]/div[3]/div/div"));
+				.findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div/div/div[2]/div[3]/div/div"));
 		Hab.click();
 		WebElement HabAdultos = driver.findElement(
-				By.xpath("//*[@id=\"component-modals\"]/div[5]/div/div/div[1]/div[2]/div[1]/div[2]/div/button[2]"));
+				By.xpath("/html/body/div[6]/div[5]/div/div/div[1]/div[2]/div[1]/div[2]/div/button[2]"));
 		Thread.sleep(1000);
 		HabAdultos.click();
+		WebElement HabMenores = driver.findElement(
+				By.xpath("/html/body/div[6]/div[5]/div/div/div[1]/div[2]/div[2]/div[2]/div/button[2]"));
+		Thread.sleep(1000);
+		HabMenores.click();
+		WebElement SelectMenor = driver.findElement
+				(By.xpath("/html/body/div[6]/div[5]/div/div/div[1]/div[2]/div[3]/div[2]/div/div/select"));
+		Thread.sleep(1000);
+		SelectMenor.click();
+		WebElement EdadMenor9 = driver.findElement
+				(By.xpath("/html/body/div[6]/div[5]/div/div/div[1]/div[2]/div[3]/div[2]/div/div/select/option[11]"));
+		Thread.sleep(1000);
+		EdadMenor9.click();
 		WebElement aplicarHab = driver
-				.findElement(By.xpath("//*[@id=\"component-modals\"]/div[5]/div/div/div[2]/a[1]"));
+				.findElement(By.xpath("/html/body/div[6]/div[5]/div/div/div[2]/a[1]/em"));
 		aplicarHab.click();
 		WebElement aplicarTodo = driver
-				.findElement(By.xpath("//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div[2]/button/em"));
+				.findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div/div/div[2]/button/em"));
 		aplicarTodo.click();
 		Thread.sleep(2000);
 		WebElement VerDetalles = driver.findElement(By.xpath(
 				"/html/body/aloha-app-root/aloha-results/div/div/div/div[2]/div[2]/aloha-list-view-container/div[2]/div[1]/aloha-cluster-container/div/div/div[2]/aloha-cluster-pricebox-container/div/div[2]/div[2]/aloha-button/button/em"));
 		VerDetalles.click();
 		Thread.sleep(3000);
+		Assert.assertTrue(VerDetalles.isDisplayed());
 		driver.quit();
+		
 	}
 }
